22582ca86c3529585347a3aaa81b429264ae653d
Rupa830904 <rupa.kabyik@gmail.com>
2023-03-28 23:04:08 +0000

    Add final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 runGame(gameType);
             }
         })
-        };
-    });
+        }
+
+        document.getElementById("answer-box").addEventListener("keydown", function(event ) {
+            if (event.key === "Enter") {
+                checkAnswer();
+            }
+        })
+    })
+
+   
 
     runGame("addition");
 
@@ -25,7 +33,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function runGame(gameType){
-
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
     //Creat two random numbers between 1 and 25
 let num1 = Math.floor(Math.random()*25)+1;
 let num2 = Math.floor(Math.random()*25)+1;
